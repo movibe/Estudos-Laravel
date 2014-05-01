23c1d4133a63d97f8e7d7471f4d9a321bed8fa6f
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-05-01 07:13:51 -0300

    Novo tutorial: http://goo.gl/KOkVfo

--- app/routes.php
+++ app/routes.php
@@ -12,40 +12,6 @@
 */
 
 Route::get('/', function(){
-		
-	# Consultas com o model product
-
-	# Criar Produto
-	// $produto = new Product;
-	// $produto->name = 'Barco';
-	// $produto->save();
-	
-	# Todos os Produtos
-	// $produtos = Product::all();
-
-	# Busca de Produto por ID
-	// $produtos = Product::find(1);
-
-	// echo $produtos;
-
-
-
-	# Consultas com Queries
-
-	# Todos os produtos de uma tabela
-	// $produtos = DB::table('products')->get();
-
-	# Inner join
-	// $produtos = DB::table('products')
-	// 		->join('product_models', 'products.id','=' ,'product_models.product_id')
-	// 		->get();
-	
-	# Left join
-	$produtos = DB::table('products')
-			->leftjoin('product_models', 'products.id','=' ,'product_models.product_id')
-			->get();
-
-	// Echo 
-	print_r($produtos);
+	return View::make('hello');
 
 });
